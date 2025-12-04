02a80cfa79b826e06a288390bdb836dfeb0a2919
Ebn Sina <ebnsina@Ebns-MacBook-Pro.local>
2025-12-04 23:38:05 +0600

    add cloudflare adapter

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,16 +5,24 @@ declare global {
     // interface Error {}
     interface Locals {
       user?: {
-        id: string;
-        email: string;
-        name: string | null;
-        plan: string;
-      };
+        id: string
+        email: string
+        name: string | null
+        plan: string
+      }
     }
     // interface PageData {}
     // interface PageState {}
-    // interface Platform {}
+    interface Platform {
+      env: {
+        COUNTER: DurableObjectNamespace
+      }
+      context: {
+        waitUntil(promise: Promise<any>): void
+      }
+      caches: CacheStorage & { default: Cache }
+    }
   }
 }
 
-export {};
+export {}
